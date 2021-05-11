6306e1667fb970656ef5758299113106c284ce74
RAF7519 <aklilmarouf@gmail.com>
2021-05-11 12:11:56 +0200

    exo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log(coucou)
+// On importe la libraire fastify
+import fastify from 'fastify'
+
+// On créé une application fastify en utilisant
+// L'import de notre librairie. On configure
+// fastify pour afficher des logs
+const app = fastify({ logger: true })
+
+app . get ( '/' ,  async  ( )  =>  {
+    return  {  text : " Génial, c'était facile.l'écriture d'une application complexe nécessite beaucoup plus de code que cet exemple. "  }
+  } )
+
+// Cette fonction démarre notre server d'api
+const start = async () => {
+  console.log('Lancement de notre server ...')
+
+  await app.listen(3000)
+
+  console.log('Le server est lancé, vous pouvez visiter: http://localhost:3000')
+}
+
+// Lancement de la fonction de démarage
+start()
